46268ea280ca522c6447a4b84db1402b51c01984
ahmedmidopro <46852690+ahmedmidopro@users.noreply.github.com>
2019-01-20 00:43:26 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("536302457014452226")
+setInterval(function() {
+channel.send(`فكك ملكش فية :) :* :)`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
